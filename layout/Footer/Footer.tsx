88621cfdb3e0444f24a1dbabffe3311180972782
Petr <67069483+Slick365@users.noreply.github.com>
2022-05-18 20:56:22 +0300

    Complite general layout

--- layout/Footer/Footer.tsx
+++ layout/Footer/Footer.tsx
@@ -1,7 +1,18 @@
 import { FooterProps } from './Footer.props';
-import style from './paragraph.module.css';
+import style from './Footer.module.css';
 import cn from 'classnames';
+import { format } from 'date-fns';
 
-export const Footer = ({ ...props }: FooterProps): JSX.Element => {
-  return <div {...props}>Footer</div>;
+export const Footer = ({ className, ...props }: FooterProps): JSX.Element => {
+  return (
+    <footer className={cn(className, style.footer)} {...props}>
+      <div>All rights reserved {format(new Date(), 'yyyy')}</div>
+      <a href="#" target="_blank">
+        Terms and Conditions
+      </a>
+      <a href="#" target="_blank">
+        Privacy Policy
+      </a>
+    </footer>
+  );
 };
